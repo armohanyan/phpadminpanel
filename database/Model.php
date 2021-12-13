a4dc9bf026e002bcdcc310ebf2beb01f38f23464
armohanyan <ohanyyanarmen@gmail.com>
2021-12-13 14:31:56 +0400

    add database

--- database/Model.php
+++ database/Model.php
@@ -1,20 +1,41 @@
 <?php
+
 class Model
 {
-    private $servername  = 'localhost';
-    private $username = 'root';
-    private $password  = 'p7omkwud+';
-    private $database = 'admin-panel-php';
-    private $conn;
+    // private $servername  = 'localhost';
+    // private $username = 'root';
+    // private $password  = 'p7omkwud+';
+    // private $database = 'admin-panel-php';
+    
+    // public $cleardb_url; = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    // private $cleardb_server = $this->$cleardb_url["us-cdbr-east-05.cleardb.net"];
+    // private $cleardb_username = $this->$cleardb_url["b59a852b277dc4"];
+    // private $cleardb_password = $this->$cleardb_url["858ba862"];
+    // private $cleardb_db = substr($this->$cleardb_url["mysql://b59a852b277dc4:858ba862@us-cdbr-east-05.cleardb.net/heroku_fae88e21dd79ac0?reconnect=true"], 1);
+    // private $active_group = 'default';
+    // private $query_builder = TRUE;
 
-    public function __construct()
+    public $conn;
+
+    public function __construct($conn = '')
     {
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+        $cleardb_username = $cleardb_url["b59a852b277dc4"];
+        $cleardb_password = $cleardb_url["858ba862"];
+        $cleardb_db = substr($cleardb_url["mysql://b59a852b277dc4:858ba862@us-cdbr-east-05.cleardb.net/heroku_fae88e21dd79ac0?reconnect=true"],1);
+        
+        $active_group = 'default';
+        $query_builder = TRUE;
+        
+        $this->$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        
+        $this->$conn = $conn;
 
-        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
-
-        if (!$this->conn) {
+        if ($this->$conn) {
             die("Connection failed:" . mysqli_connect_error());
         }
+
     }
 
     public function getConnect()
