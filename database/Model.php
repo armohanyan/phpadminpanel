c0ef8aeb31c0eb0be2a0aeacd2cc780e5db6f632
armohanyan <ohanyyanarmen@gmail.com>
2021-12-13 17:32:07 +0400

    add database

--- database/Model.php
+++ database/Model.php
@@ -4,18 +4,24 @@ class Model
 {
 
     public function getConnect()
-    {   
+    {
         // $servername  = 'localhost';
         // $username = 'root';
         // $password  = 'p7omkwud+';
         // $database = 'admin-panel-php';
 
-        $cleardb_url = parse_url(getenv("mysql://b8e2a3fc0959b8:a83b86eb@us-cdbr-east-05.cleardb.net/heroku_cb2598b8971e8f8?reconnect=true"));
-        $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
-        $cleardb_username = $cleardb_url["b8e2a3fc0959b8"];
-        $cleardb_password = $cleardb_url["a83b86eb"];
-        $cleardb_db = substr($cleardb_url["heroku_cb2598b8971e8f8"], 1);
+        // $cleardb_url = parse_url(getenv("mysql://b8e2a3fc0959b8:a83b86eb@us-cdbr-east-05.cleardb.net/heroku_cb2598b8971e8f8?reconnect=true"));
+        // $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+        // $cleardb_username = $cleardb_url["b8e2a3fc0959b8"];
+        // $cleardb_password = $cleardb_url["a83b86eb"];
+        // $cleardb_db = substr($cleardb_url["heroku_cb2598b8971e8f8"], 1);
 
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"], 1);
+        
         $active_group = 'default';
         $query_builder = TRUE;
 
