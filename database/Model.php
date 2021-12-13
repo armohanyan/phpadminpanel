6e48b798f0ae66c1859240bea97dc379b2647640
armohanyan <ohanyyanarmen@gmail.com>
2021-12-13 15:50:25 +0400

    add composer

--- database/Model.php
+++ database/Model.php
@@ -23,7 +23,7 @@ class Model
         $cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
         $cleardb_username = $cleardb_url["b8e2a3fc0959b8"];
         $cleardb_password = $cleardb_url["a83b86eb"];
-        $cleardb_db = substr($cleardb_url["mysql://b8e2a3fc0959b8:a83b86eb@us-cdbr-east-05.cleardb.net/heroku_cb2598b8971e8f8?reconnect=true"],1);
+        $cleardb_db = substr($cleardb_url["admin-panel-php"],1);
         
         $active_group = 'default';
         $query_builder = TRUE;
