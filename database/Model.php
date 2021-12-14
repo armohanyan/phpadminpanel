d19f969bc96db5f9c364fb42c3521cfe302b7a22
armohanyan <ohanyyanarmen@gmail.com>
2021-12-14 14:39:26 +0400

    updates

--- database/Model.php
+++ database/Model.php
@@ -5,23 +5,25 @@ class Model
 
     public function getConnect()
     {
+        // for local 
          $servername  = 'localhost';
          $username = 'root';
          $password  = 'p7omkwud+';
          $database = 'admin-panel-php';
-
+         
+         $conn = mysqli_connect($servername, $username, $password, $database);
+        
+         // for heroku 
         // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         // $cleardb_server = $cleardb_url["host"];
         // $cleardb_username = $cleardb_url["user"];
         // $cleardb_password = $cleardb_url["pass"];
         // $cleardb_db = substr($cleardb_url["path"], 1);
-
         // $active_group = 'default';
         // $query_builder = TRUE;
 
         //  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-        $conn = mysqli_connect($servername, $username, $password, $database);
 
         if (!$conn) {
             die("Connection failed:" . mysqli_connect_error());
