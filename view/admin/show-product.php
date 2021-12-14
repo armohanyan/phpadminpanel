81d9da0055b3eeb9fd11c1d9b27cd8196ee2e0d7
armohanyan <ohanyyanarmen@gmail.com>
2021-12-14 15:56:44 +0400

    updates

--- view/admin/show-product.php
+++ view/admin/show-product.php
@@ -9,9 +9,11 @@ $productAndReviews = $productController->show($_GET['id']);
 // print_r($productAndReviews);
 // echo '</pre>';
 $product = $productAndReviews[0];
-$reviews = array_reverse($productAndReviews[1]);
-$countOfStars = $productAndReviews[2];
-$avgRating = $productAndReviews[3];
+if ($productAndReviews[1] != null) {
+    $reviews = array_reverse($productAndReviews[1]);
+    $countOfStars = $productAndReviews[2];
+    $avgRating = $productAndReviews[3];
+}
 
 ?>
 
@@ -283,131 +285,136 @@ $avgRating = $productAndReviews[3];
                 </div>
             </div>
         </div>
-        <div class="container">
+        <?php
 
-            <div class="row">
-                <div class="col-sm-3">
-                    <div class="rating-block">
-                        <h4>Average user rating</h4>
-                        <h2 class="bold padding-bottom-7"><?php echo $avgRating  ?><small>/ 5</small></h2>
-                        <?php for ($i = 0; $i < round($avgRating); $i++) { ?>
-                            <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
-                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                            </button>
-                        <?php } ?>
-                        <?php for ($i = round($avgRating); $i < 5; $i++) { ?>
-                            <button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
-                                <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                            </button>
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="col-sm-3">
-                    <h4>Rating breakdown</h4>
-                    <div class="pull-left">
-                        <div class="pull-left" style="width:35px; line-height:1;">
-                            <div style="height:9px; margin:5px 0;">5 <span class="glyphicon glyphicon-star"></span></div>
-                        </div>
-                        <div class="pull-left" style="width:180px;">
-                            <div class="progress" style="height:9px; margin:8px 0;">
-                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="5" style="width: 1000%">
-                                    <span class="sr-only">80% Complete (danger)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[5] ?></div>
-                    </div>
-                    <div class="pull-left">
-                        <div class="pull-left" style="width:35px; line-height:1;">
-                            <div style="height:9px; margin:5px 0;">4 <span class="glyphicon glyphicon-star"></span></div>
-                        </div>
-                        <div class="pull-left" style="width:180px;">
-                            <div class="progress" style="height:9px; margin:8px 0;">
-                                <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="4" aria-valuemin="0" aria-valuemax="5" style="width: 80%">
-                                    <span class="sr-only">80% Complete (danger)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[4] ?></div>
-                    </div>
-                    <div class="pull-left">
-                        <div class="pull-left" style="width:35px; line-height:1;">
-                            <div style="height:9px; margin:5px 0;">3 <span class="glyphicon glyphicon-star"></span></div>
-                        </div>
-                        <div class="pull-left" style="width:180px;">
-                            <div class="progress" style="height:9px; margin:8px 0;">
-                                <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="5" style="width: 60%">
-                                    <span class="sr-only">80% Complete (danger)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[3] ?></div>
-                    </div>
-                    <div class="pull-left">
-                        <div class="pull-left" style="width:35px; line-height:1;">
-                            <div style="height:9px; margin:5px 0;">2 <span class="glyphicon glyphicon-star"></span></div>
-                        </div>
-                        <div class="pull-left" style="width:180px;">
-                            <div class="progress" style="height:9px; margin:8px 0;">
-                                <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="5" style="width: 40%">
-                                    <span class="sr-only">80% Complete (danger)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[2] ?></div>
-                    </div>
-                    <div class="pull-left">
-                        <div class="pull-left" style="width:35px; line-height:1;">
-                            <div style="height:9px; margin:5px 0;">1 <span class="glyphicon glyphicon-star"></span></div>
-                        </div>
-                        <div class="pull-left" style="width:180px;">
-                            <div class="progress" style="height:9px; margin:8px 0;">
-                                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="5" style="width: 20%">
-                                    <span class="sr-only">80% Complete (danger)</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[1] ?></div>
-                    </div>
-                </div>
-            </div>
+        if ($productAndReviews[1] != null) { ?>
 
-            <div class="row">
-                <div class="col-sm-7">
-                    <hr>
-                    <?php
-                    if ($reviews) {
-                        foreach ($reviews as $review) { ?>
-                            <div class="review-block">
-                                <div class="row">
-                                    <div class="col-sm-3">
-                                        <img src="https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png" class="img-rounded">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="rating-block">
+                            <h4>Average user rating</h4>
+                            <h2 class="bold padding-bottom-7"><?php echo $avgRating  ?><small>/ 5</small></h2>
+                            <?php for ($i = 0; $i < round($avgRating); $i++) { ?>
+                                <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+                                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                </button>
+                            <?php } ?>
+                            <?php for ($i = round($avgRating); $i < 5; $i++) { ?>
+                                <button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+                                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                </button>
+                            <?php } ?>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <h4>Rating breakdown</h4>
+                        <div class="pull-left">
+                            <div class="pull-left" style="width:35px; line-height:1;">
+                                <div style="height:9px; margin:5px 0;">5 <span class="glyphicon glyphicon-star"></span></div>
+                            </div>
+                            <div class="pull-left" style="width:180px;">
+                                <div class="progress" style="height:9px; margin:8px 0;">
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="5" style="width: 1000%">
+                                        <span class="sr-only">80% Complete (danger)</span>
                                     </div>
-                                    <div class="col-sm-9">
-                                        <div class="review-block-title"><?php echo $review['username'] ?></div>
-                                        <div class="review-block-rate">
-                                            <?php for ($i = 0; $i < $review['stars']; $i++) { ?>
-                                                <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
-                                                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                                                </button>
-                                            <?php } ?>
-                                            <?php for ($i = $review['stars']; $i < 5; $i++) { ?>
-                                                <button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
-                                                    <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                                                </button>
-                                            <?php } ?>
+                                </div>
+                            </div>
+                            <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[5] ?></div>
+                        </div>
+                        <div class="pull-left">
+                            <div class="pull-left" style="width:35px; line-height:1;">
+                                <div style="height:9px; margin:5px 0;">4 <span class="glyphicon glyphicon-star"></span></div>
+                            </div>
+                            <div class="pull-left" style="width:180px;">
+                                <div class="progress" style="height:9px; margin:8px 0;">
+                                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="4" aria-valuemin="0" aria-valuemax="5" style="width: 80%">
+                                        <span class="sr-only">80% Complete (danger)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[4] ?></div>
+                        </div>
+                        <div class="pull-left">
+                            <div class="pull-left" style="width:35px; line-height:1;">
+                                <div style="height:9px; margin:5px 0;">3 <span class="glyphicon glyphicon-star"></span></div>
+                            </div>
+                            <div class="pull-left" style="width:180px;">
+                                <div class="progress" style="height:9px; margin:8px 0;">
+                                    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="5" style="width: 60%">
+                                        <span class="sr-only">80% Complete (danger)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[3] ?></div>
+                        </div>
+                        <div class="pull-left">
+                            <div class="pull-left" style="width:35px; line-height:1;">
+                                <div style="height:9px; margin:5px 0;">2 <span class="glyphicon glyphicon-star"></span></div>
+                            </div>
+                            <div class="pull-left" style="width:180px;">
+                                <div class="progress" style="height:9px; margin:8px 0;">
+                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="5" style="width: 40%">
+                                        <span class="sr-only">80% Complete (danger)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[2] ?></div>
+                        </div>
+                        <div class="pull-left">
+                            <div class="pull-left" style="width:35px; line-height:1;">
+                                <div style="height:9px; margin:5px 0;">1 <span class="glyphicon glyphicon-star"></span></div>
+                            </div>
+                            <div class="pull-left" style="width:180px;">
+                                <div class="progress" style="height:9px; margin:8px 0;">
+                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="5" style="width: 20%">
+                                        <span class="sr-only">80% Complete (danger)</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pull-right" style="margin-left:10px;"><?php echo $countOfStars[1] ?></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-7">
+                        <hr>
+                        <?php
+                        if ($reviews) {
+                            foreach ($reviews as $review) { ?>
+                                <div class="review-block">
+                                    <div class="row">
+                                        <div class="col-sm-3">
+                                            <img src="https://icons-for-free.com/iconfiles/png/512/business+costume+male+man+office+user+icon-1320196264882354682.png" class="img-rounded">
                                         </div>
+                                        <div class="col-sm-9">
+                                            <div class="review-block-title"><?php echo $review['username'] ?></div>
+                                            <div class="review-block-rate">
+                                                <?php for ($i = 0; $i < $review['stars']; $i++) { ?>
+                                                    <button type="button" class="btn btn-warning btn-xs" aria-label="Left Align">
+                                                        <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                                    </button>
+                                                <?php } ?>
+                                                <?php for ($i = $review['stars']; $i < 5; $i++) { ?>
+                                                    <button type="button" class="btn btn-default btn-grey btn-xs" aria-label="Left Align">
+                                                        <span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+                                                    </button>
+                                                <?php } ?>
+                                            </div>
 
-                                        <div class="review-block-description"><?php echo $review['comment'] ?></div>
+                                            <div class="review-block-description"><?php echo $review['comment'] ?></div>
+                                        </div>
                                     </div>
+                                    <hr>
                                 </div>
-                                <hr>
-                            </div>
-                    <?php }
-                    } ?>
+                        <?php }
+                        } ?>
+                    </div>
                 </div>
             </div>
-        </div>
+        <?php } ?>
     </section>
     <script src="../../resource/js/admin-js.js"></script>
     <script sr="../../resource/js/main.js"></script>
